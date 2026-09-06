1334098186710860acac5c542dd83655889d51b4
Howard Edwards <howarde.edwards@gmail.com>
2026-09-06 14:48:04 -0400

    fix: attach option-defined hooks immediately (#30)
    * feat: complete hooks and TypeScript support
    
    * revert: defer TypeScript declarations

--- test/unit/sequelize.test.js
+++ test/unit/sequelize.test.js
@@ -7,6 +7,31 @@ describe('src/sequelize', () => {
     expect(typeof sequelize.define).toBe('function')
   })
 
+  it('attaches option-defined hooks immediately', () => {
+    const beforeValidate = jest.fn()
+    const Model = sequelize.define('Hooked', {}, { hooks: { beforeValidate } })
+
+    expect(new Model().hooks.beforeValidate).toBe(beforeValidate)
+  })
+
+  it('copies the option-defined hook map', () => {
+    const beforeValidate = jest.fn()
+    const optionHooks = { beforeValidate }
+    const Model = sequelize.define('Hooked', {}, { hooks: optionHooks })
+
+    expect(Model.prototype.hooks).not.toBe(optionHooks)
+  })
+
+  it('does not mutate option-defined hooks when registering more hooks', () => {
+    const beforeValidate = jest.fn()
+    const optionHooks = { beforeValidate }
+    const Model = sequelize.define('Hooked', {}, { hooks: optionHooks })
+
+    Model.addHook('afterValidate', jest.fn())
+
+    expect(optionHooks).toEqual({ beforeValidate })
+  })
+
   staticMethods.forEach(method => {
     it(`has static method ${method}`, () => {
       expect(typeof sequelize[method]).toBe('function')
